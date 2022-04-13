6069a5210f83eef970fa41a3519c00e7c65b8d3f
Manuel Schmid <manysch3@gmail.com>
2022-04-13 15:36:51 +0200

    added home screen, added logo

---
+++ pages/home.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Olma | Home</title>
+    <link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+
+</body>
+</html>
