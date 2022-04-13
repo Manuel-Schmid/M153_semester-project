5e4e79e6f989d000943902215a99a7d858d19689
padischer <sven.walser26@gmail.com>
2022-04-13 23:16:11 +0200

    added login button and answer buttons on home page and made login page

---
+++ pages/adminLogin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Olma | Admin-Login</title>
+    <link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+
+<header>
+    <h1>Admin-Login</h1>
+</header>
+
+<form action="adminLogin.php" method="post">
+    <label>Password: </label>
+    <input type="text">
+    <br>
+    <br>
+    <input type="button" value="submit">
+</form>
+
+</body>
+</html>
+
---
+++ pages/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Olma | Home</title>
+    <link rel="stylesheet" href="../styles.css">
+</head>
+<body id="home_body">
+<header>
+    <form action="home.php" method="post">
+        <a id="loginButton" href="adminLogin.php"><input type="button" name="Login" value="Login"></a>
+    </form>
+</header>
+
+
+
+<h1>Question?</h1>
+<form action="home.php" method="post" id="questions">
+    <input type="button" name="Answer1" value="Answer 1">
+    <input type="button" name="Answer2" value="Answer 2">
+    <input type="button" name="Answer3" value="Answer 3">
+    <input type="button" name="Answer4" value="Answer 4">
+</form>
+
+</body>
+</html>
