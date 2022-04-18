81c1ef282c7f0e39c32065d4ca6fe942f4204ff9
padischer <sven.walser26@gmail.com>
2022-04-18 21:56:45 +0200

    fixed an issue where the sidebar would move after a reload and adjusted it's design

--- pages/adminQuestion.php
+++ pages/adminQuestion.php
@@ -11,21 +11,21 @@
             <p>Question</p>
             <input type="text" name="Question">
             <p>Anwser 1</p>
-            <input type="text" name="Question">
+            <input type="text" name="Answer1">
             <p>Anwser 2</p>
-            <input type="text" name="Question">
+            <input type="text" name="Answer2">
             <p>Anwser 3</p>
-            <input type="text" name="Question">
+            <input type="text" name="Answer3">
             <p>Anwser 4</p>
-            <input type="text" name="Question">
+            <input type="text" name="Answer4">
         </form>
     </section>
 
     <section class="admin_sidebar">
-        <a href="adminQuestion.php" class="currentPage">Questions</a><br>
-        <a href="adminGallery.php">Gallery</a><br>
-        <a href="adminData.php">Date</a><br>
-        <a href="adminWinner.php">Winner</a><br>
+        <a href="adminQuestion.php"><div id="currentPage" class="sidebarItem">Questions</div></a>
+        <a href="adminGallery.php"><div class="sidebarItem">Gallery</div></a>
+        <a href="adminData.php"><div class="sidebarItem">Date</div></a>
+        <a href="adminWinner.php"><div class="sidebarItem">Winner</div></a>
     </section>
 
 
