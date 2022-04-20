88e8c08a582fd8f1e8cf49646db92f6401f670e7
padischer <sven.walser26@gmail.com>
2022-04-20 19:07:46 +0200

    finished sidebar, applied changes to other admin pages and adjusted design of adminQuestion content

--- pages/adminQuestion.php
+++ pages/adminQuestion.php
@@ -8,24 +8,25 @@
 <body id="admin-view_body">
     <section class="admin_content">
         <form action="adminQuestion.php" action="post">
-            <p>Question</p>
-            <input type="text" name="Question">
-            <p>Anwser 1</p>
-            <input type="text" name="Answer1">
-            <p>Anwser 2</p>
-            <input type="text" name="Answer2">
-            <p>Anwser 3</p>
-            <input type="text" name="Answer3">
-            <p>Anwser 4</p>
-            <input type="text" name="Answer4">
+            <br>
+            <h1> Create a new Question </h1>
+            <br>
+            <p>Question: <input type="text" name="Question"></p>
+            <p>Anwser 1: <input type="text" name="Answer1"></p>
+            <p>Anwser 2: <input type="text" name="Answer2"></p>
+            <p>Anwser 3: <input type="text" name="Answer3"></p>
+            <p>Anwser 4: <input type="text" name="Answer4"></p>
+            <br>
+            <input type="button" value=" submit " name="submitQuestion">
         </form>
     </section>
 
     <section class="admin_sidebar">
-        <a href="adminQuestion.php"><div id="currentPage" class="sidebarItem">Questions</div></a>
-        <a href="adminGallery.php"><div class="sidebarItem">Gallery</div></a>
-        <a href="adminData.php"><div class="sidebarItem">Date</div></a>
-        <a href="adminWinner.php"><div class="sidebarItem">Winner</div></a>
+        <a href="adminQuestion.php" class="sidebarRemoveLink"><div id="currentPage" class="sidebarItem">Questions</div></a>
+        <a href="adminGallery.php" class="sidebarRemoveLink"><div class="sidebarItem">Gallery</div></a>
+        <a href="adminData.php" class="sidebarRemoveLink"><div class="sidebarItem">Date</div></a>
+        <a href="adminWinner.php" class="sidebarRemoveLink"><div class="sidebarItem">Winner</div></a>
+        <a href="home.php" class="sidebarRemoveLink"><div class="sidebarItem">Logout</div></a>
     </section>
 
 
