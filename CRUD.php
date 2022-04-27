0a58a3c0ba3f8dc24e2aa7e316e90ba2fd986eed
padischer <sven.walser26@gmail.com>
2022-04-27 20:07:56 +0200

    user table will now be visible once opening adminData.php *needs finetuning*

--- CRUD.php
+++ CRUD.php
@@ -12,3 +12,24 @@ function checkAdminPW($pw): int {
     global $adminPWD;
     return $pw == $adminPWD;
 }
+
+function getAllUsers(): array {
+    global $pdo;
+    $query = $pdo->prepare('SELECT * FROM user;');
+    $query->execute();
+    return $query->fetchAll();
+}
+/*
+$query = $con->prepare('INSERT INTO spieler (vorname, nachname, highscore) VALUES (?,?,?);');
+$query->bindValue(1, $_POST['firstname']);
+$query->bindValue(2, $_POST['lastname']);
+$query->bindValue(3, $_POST['highscore']);
+$query->execute();
+
+$query = $con->prepare('DELETE FROM spieler WHERE highscore=0');
+$query->execute();
+
+$sql = "SELECT * FROM spieler ORDER BY highscore DESC";
+$stmt = $con->query($sql);
+$players = $stmt->fetchAll();
+*/
