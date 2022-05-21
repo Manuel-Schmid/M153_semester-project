b302b46ffcc1493ccc314c60527c724160111aa3
Lewin <Lewin.rutz@gmail.com>
2022-05-21 10:39:08 +0200

    added the thanks screen.

--- pages/NoteOfThanks.php
+++ pages/NoteOfThanks.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Refresh" content="10; url='home.php'" />
+    <title>Olma | Home</title>
+    <link rel="stylesheet" href="../styles.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+<img src="../assets/Logo-M153.svg" alt="" width="150">
+<div class="containerThanks">
+    <h1>Danke viel mals für das Mitmachen!</h1>
+    <h2>Wir wünschen Ihnen viel Glück!</h2>
+</div>
+</body>
+</html>
